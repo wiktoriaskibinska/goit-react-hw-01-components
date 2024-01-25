90a9ab70dc1287aefed157d9a5781a6c45d345f7
Wiktoria Skibińska <wikas2000@icloud.com>
2024-01-25 17:55:49 +0100

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Profile from './Profile/profile';
 import user from './Profile/user.json';
 import Statistics from './Statistics/statistics';
 import data from './Statistics/data.json';
-import FriendList from './FriendList/friendList';
+import FriendList from './FriendList/friendlist';
 import friends from './FriendList/friends.json';
 import TransactionsHistory from './Transactions/transactionsHistory';
 import transactions from './Transactions/transactions.json';
